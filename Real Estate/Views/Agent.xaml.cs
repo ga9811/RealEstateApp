5f91487dcdd2a0d3939b4767365758adfc3a3f84
ga9811 <ga98100@gmail.com>
2023-07-25 12:21:56 -0400

    all here

---
+++ Real Estate/Views/Agent.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Real_Estate
+{
+    /// <summary>
+    /// Interaction logic for Agent.xaml
+    /// </summary>
+    public partial class Agent : Window
+    {
+        public Agent()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DateTimePicker_SelectedDateTimeChanged(object sender, RoutedPropertyChangedEventArgs<DateTime?> e)
+        {
+
+        }
+
+        private void DateTimePicker_SelectedDateTimeChanged_1(object sender, RoutedPropertyChangedEventArgs<DateTime?> e)
+        {
+
+        }
+
+        private void DateTimePicker_SelectedDateTimeChanged_2(object sender, RoutedPropertyChangedEventArgs<DateTime?> e)
+        {
+
+        }
+    }
+}
